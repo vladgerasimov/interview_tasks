15ed73caedca6adf07494f0ed04adf83b2ef53c2
vlad <lector45rus@gmail.com>
2021-03-14 15:47:37 +0300

    Task is done properly

--- yandex_task.py
+++ yandex_task.py
@@ -12,21 +12,18 @@ from math import gcd
 def calculate_max_friends(candies_list):
     """
     Calculates maximum number of friends I can present the same set of candies with no candies left for myself.
-    Firstly, I calculate the number of candies of each unique sort. Then I extract unique numbers of the same candies
-    sort. Last, I check if there is a common divisor of these values that is not 1, if so, I can make same sets of
-    candies for minimal value in this set. If common divisor is 1 then I must give all candies to just one friend.
+    Firstly, I calculate the number of candies of each unique sort. Lastly, I return the greatest common divisor
+    for these number of candies of each unique sort. That's the answer, it's the maximum number of friends I can
+    present the same sets of candies with no candies left for myself
     :param candies_list:
     :return int:
     """
-    candy_duplicates = {i: candies_list.count(i) for i in set(candies_list)}
-    uniq_duplicates = set(candy_duplicates.values())
-    if gcd(*uniq_duplicates) == 1:
-        return 1
-    return min(uniq_duplicates)
+    candy_duplicates = [candies_list.count(i) for i in set(candies_list)]
+    return gcd(*candy_duplicates)
 
 
 if __name__ == '__main__':
-    candies = ['a', 'a', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'b', 'c', 'c', 'a', 'a', 'a', 'b', 'b', 'c', 'b']
+    candies = ['a', 'a', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'b']
 
     assert calculate_max_friends(['a', 'b', 'c', 'a', 'b', 'c']) == 2
     assert calculate_max_friends(['a', 'b', 'c', 'a', 'b', 'c', 'd']) == 1
